ed4fe0ea60aa803b5e92ec4d56ccf929b801cdad
aakif-abrar <aakifabrar74@gmail.com>
2025-01-27 23:11:20 +0500

    Create q4.cpp

---
+++ Semester-2/Assignment-1/q4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    float kg;
+    cout<<"Enter weight in kg: ";
+    cin>>kg;
+    printf("Weight in pounds %.2f",kg/2.2);
+}
